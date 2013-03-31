78ef8ad166cd628177f1be08c56f072045ba5ec5
timcking <timcking@gmail.com>
2013-03-30 19:40:40 -0700

    Basics

--- LoadCheckList.py
+++ LoadCheckList.py
@@ -5,8 +5,9 @@ if __name__ == '__main__':
     accessData = PyAccess()    
     
     # Insert some rows
-    args = ('Tim Top', 4, 'HR', 'A ref')
-    accessData.insert_chklist(args)    
+    # args = ('Tim Top', 4, 'HR', 'A ref')
+    # newID = accessData.insert_chklist(args)    
+    # print ("New Checklist = %s" % (newID))
     
     # Run a query
     rows = accessData.get_chklists('HR')
@@ -15,6 +16,19 @@ if __name__ == '__main__':
     
     for chklst_id in rows:
         print chklst_id[0]
+    
+    # Update
+    count = accessData.update_chklist('101', 'CM')
+    print ("%s rows updated" % (count))
+        
+    # Delete
+    # count = accessData.delete_chklist(100)
+    # print ("%s rows deleted" % (count))
+    
+    # Query Excel using a named range
+    rows = accessData.query_excel()
+    for exData in rows:
+        print exData
     
     # Cleanup
     accessData.close_cursor()
--- PyAccess.py
+++ PyAccess.py
@@ -8,12 +8,46 @@ class PyAccess():
     def __init__(self):
         pass
     
+    def query_excel(self):
+        DSN = 'Driver={Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};DBQ=.\unit_tests.xlsx;'
+        exConn = pyodbc.connect(DSN, autocommit = True)
+        exCursor = exConn.cursor()
+        
+        sql = "SELECT * from tblItems"
+        
+        exCursor.execute(sql, )
+        rows = exCursor.fetchall()
+        
+        exCursor.close()
+        exConn.close()
+        return rows
+    
+    def delete_chklist(self, chklst_id):
+        sql = "DELETE FROM tblChkLst WHERE chklst_id = ?;"
+        count = self.cursor.execute(sql, (chklst_id, )).rowcount
+        self.conn.commit()
+        return count
+        
+    def update_chklist(self, chklst_id, area_code):
+        sql = "UPDATE tblChkLst " +\
+              "SET area_code = ? " +\
+              "WHERE chklst_id = ?;"
+        count = self.cursor.execute(sql, (area_code, chklst_id, )).rowcount
+        self.conn.commit()
+        return count
+        
     def insert_chklist(self, args):
         sql = "INSERT INTO tblChkLst(chklst_name, template_id, area_code, reference) " +\
               "VALUES (?, ?, ?, ?)"
         
         self.cursor.execute(sql, args)
         self.conn.commit()
+        
+        # Get ID of the one we just inserted
+        sql = "SELECT MAX(chklst_id) FROM tblChkLst;"
+        self.cursor.execute(sql, )
+        row = self.cursor.fetchone()
+        return row[0]
         
     def get_chklists(self, area):
         sql = "SELECT chklst_id " +\
