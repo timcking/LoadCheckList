0466db20969c707eae3c1cdea334b287486f04ee
Tim King <timcking@gmail.com>
2013-04-03 17:38:44 -0700

    Work in progress

--- LoadCheckList.py
+++ LoadCheckList.py
@@ -2,46 +2,46 @@ import sys
 from PyAccess import PyAccess
 
 if __name__ == '__main__':
-    accessData = PyAccess()    
-    
+    accessData = PyAccess()
+
     # print "*************** Insert New Checklist ***************"
     # args = ('Tim Top', 4, 'HR', 'A ref')
-    # newID = accessData.insert_chklist(args)    
+    # newID = accessData.insert_chklist(args)
     # print ("New Checklist = %s" % (newID))
-    
-    print "**************** Get Headers ***********************"
-    rows = accessData.get_excel_headers()
-    for row in rows:
-        print ("Seq: %d, Text: %s" % (row[0], row[1]))
-    
+
+    #print "**************** Get Headers ***********************"
+    #rows = accessData.get_excel_headers()
+    #for row in rows:
+        #print ("Seq: %d, Text: %s" % (row[0], row[1]))
+
     # Could use this to insert into another table
     # print rows:  [(1.0, 'Professional Delivery'), (2.0, 'Defects'), (3.0, 'Unit Tests')]
     # executemany("insert into tblChkLstHeader(sequence, chklst_header_text) values (?, ?)", rows)
-    
+
     print "*********** Insert a Checklist from Excel ************"
     args = accessData.get_excel_chklist()
-    newID = accessData.insert_chklist(args)    
-    print ("New Checklist = %s" % (newID))
-    
-    print "**************** Run a Query ***********************"
-    rows = accessData.get_chklists('HR')
-    iTotRecs = len(rows)
-    print ("%s found" % (iTotRecs))
-    for chklst_id in rows:
-        print chklst_id[0]
-    
-    print "*************** Update Area Code *******************"
-    count = accessData.update_areacode('101', 'RM')
-    print ("%s rows updated" % (count))
-        
-    print "*************** Delete a Row ***********************"
-    count = accessData.delete_chklist(100)
-    print ("%s rows deleted" % (count))
-    
-    print "*************** Query Excel ************************"
-    rows = accessData.get_excel_items()
-    for exData in rows:
-        print exData
-    
+    newID = accessData.insert_chklist(args)
+    print ("Last Checklist = %s" % (newID))
+
+    #print "**************** Run a Query ***********************"
+    #rows = accessData.get_chklists('HR')
+    #iTotRecs = len(rows)
+    #print ("%s found" % (iTotRecs))
+    #for chklst_id in rows:
+        #print chklst_id[0]
+
+    #print "*************** Update Area Code *******************"
+    #count = accessData.update_areacode('101', 'RM')
+    #print ("%s rows updated" % (count))
+
+    #print "*************** Delete a Row ***********************"
+    #count = accessData.delete_chklist(100)
+    #print ("%s rows deleted" % (count))
+
+    #print "*************** Query Excel ************************"
+    #rows = accessData.get_excel_items()
+    #for exData in rows:
+        #print exData
+
     # Cleanup
     accessData.close_cursor()
--- PyAccess.py
+++ PyAccess.py
@@ -5,7 +5,7 @@ class PyAccess():
     connect_str = 'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\signal_dev.accdb;'
     conn = pyodbc.connect(connect_str)
     cursor = conn.cursor()
-    
+
     # For Excel
     connect_str = 'Driver={Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};DBQ=.\unit_tests.xlsx;'
     exConn = pyodbc.connect(connect_str, autocommit = True)
@@ -13,31 +13,32 @@ class PyAccess():
 
     def __init__(self):
         pass
-    
+
     def get_excel_chklist(self):
         sql = "SELECT chklst_name, template_id, area_code, reference FROM exChkLst;"
         self.exCursor.execute(sql, )
-        row = self.exCursor.fetchone()
-        return row
-    
+        rows = self.exCursor.fetchall()
+        print rows
+        return rows
+
     def get_excel_headers(self):
         sql = "SELECT sequence, chklst_header_text FROM exHeaders;"
         self.exCursor.execute(sql, )
         row = self.exCursor.fetchall()
         return row
-        
+
     def get_excel_items(self):
         sql = "SELECT * from exItems"
         self.exCursor.execute(sql, )
         rows = self.exCursor.fetchall()
         return rows
-    
+
     def delete_chklist(self, chklst_id):
         sql = "DELETE FROM tblChkLst WHERE chklst_id = ?;"
         count = self.cursor.execute(sql, (chklst_id, )).rowcount
         self.conn.commit()
         return count
-        
+
     def update_areacode(self, chklst_id, area_code):
         sql = "UPDATE tblChkLst " +\
               "SET area_code = ? " +\
@@ -45,31 +46,30 @@ class PyAccess():
         count = self.cursor.execute(sql, (area_code, chklst_id, )).rowcount
         self.conn.commit()
         return count
-        
+
     def insert_chklist(self, args):
+        # Insert one or more
         sql = "INSERT INTO tblChkLst(chklst_name, template_id, area_code, reference) " +\
               "VALUES (?, ?, ?, ?)"
-        
-        self.cursor.execute(sql, args)
+
+        self.cursor.executemany(sql, args)
         self.conn.commit()
-        
-        # Get ID of the one we just inserted
+
+        # Get ID of last one we just inserted
         sql = "SELECT MAX(chklst_id) FROM tblChkLst;"
-        self.cursor.execute(sql, )
         row = self.cursor.fetchone()
         return row[0]
-        
+
     def get_chklists(self, area):
         sql = "SELECT chklst_id " +\
                " FROM tblChkLst " +\
                "WHERE area_code = ? " +\
                "ORDER BY chklst_id;"
-    
+
         self.cursor.execute(sql, (area, ))
         rows = self.cursor.fetchall()
         return rows
-        
+
     def close_cursor(self):
-        self.cursor.close()        
-        self.exCursor.close()        
-        
+        self.cursor.close()
+        self.exCursor.close()
